67becd2eccc24a75aadb0bc383a6170a9e53d1ec
Laura Terry <proxycharm@gmail.com>
2024-04-04 13:12:03 -0400

    first_finished_cypher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,43 @@
-# add your code here
+# Laura Terry caeser cypher code pracrtice
+
+alpha = ("abcdefghijklmnopqrstuvwxyz")
+
+print("Welcome to the Caeser Cypher!")
+begin = input("Would you like to encode (e) or decode (d) ? ").lower()
+textgiven=input("What is your text? ").lower()
+shift=int(input("Please provide the number key, choose 1-26 "))
+
+
+def encrypt(textgiven, shift):
+    encodedtext = ' '
+    for letter in textgiven:
+        index = alpha.find(letter)
+        newalpha = index + shift
+        if newalpha >= 26 :
+            newalpha -=26
+        encodedtext += alpha[newalpha]
+    return encodedtext
+
+def decrypt(textgiven, shift):
+    decodedtext = ' '
+    for letter in textgiven:
+        index = alpha.find(letter)
+        newalpha = index - shift
+        if newalpha <0 :
+            newalpha +=26
+        decodedtext += alpha[newalpha]
+    return decodedtext
+
+if begin == "e":
+    encodedtext = encrypt(textgiven, shift)
+    print (encodedtext)
+
+if begin == "d":
+    decodedtext = decrypt(textgiven, shift)
+    print(decodedtext)
+
+
+
+
+
+    
